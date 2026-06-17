1bf84c73ebb242300de5e88a10e9f2b4ceed8300
wlhabiib <wlhabiib@github.com>
2026-06-17 02:51:41 +0000

    Fix navigation bar to viewport bottom and allow content to scroll under it

--- components/app-shell.tsx
+++ components/app-shell.tsx
@@ -21,7 +21,7 @@ export default function AppShell({ children }: { children: React.ReactNode }) {
         <div className="absolute bottom-0 left-0 w-64 h-64 bg-sky-500/10 rounded-full blur-3xl translate-y-1/2 -translate-x-1/2" />
       </div>
       <Header />
-      <main className="relative pt-14 pb-24 px-4 w-full overflow-x-hidden">
+      <main className="relative pt-14 px-4 w-full overflow-x-hidden">
         {children}
       </main>
       <BottomNav />
